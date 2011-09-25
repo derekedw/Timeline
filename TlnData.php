c0f1ceb7cdadbe5e01a46c4818d229166ef3466c
Derek <derekedw@yahoo.com>
2011-09-25 17:04:44 -0400

    Main view completed

--- TlnData.php
+++ TlnData.php
@@ -107,9 +107,12 @@ class TlnData {
 			tln_source_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
 			source VARCHAR(25) NOT NULL,
 			sourcetype VARCHAR(25) NOT NULL,
-	    	type VARCHAR(50) NOT NULL,
+			M CHAR(1) NOT NULL default \'.\',
+			A CHAR(1) NOT NULL default \'.\',
+			C CHAR(1) NOT NULL default \'.\',
+			B CHAR(1) NOT NULL default \'.\',
 			PRIMARY KEY (tln_source_id),
-			UNIQUE (source, sourcetype, type)
+			UNIQUE (source, sourcetype, M, A, C, B)
 		)';
 		if ($this->db->query($sql) == TRUE) {
 			$endtime = time();
@@ -128,7 +131,6 @@ class TlnData {
 			tln_time_id BIGINT UNSIGNED NOT NULL,
 	    	tln_source_id BIGINT UNSIGNED NOT NULL,
 	    	count BIGINT UNSIGNED NOT NULL,
-	    	MACB CHAR(4) NOT NULL,
 			user VARCHAR(25) NOT NULL,
 			host VARCHAR(25) NOT NULL,
 			short VARCHAR(1000) NOT NULL,
@@ -140,7 +142,7 @@ class TlnData {
 			format VARCHAR(50) NOT NULL,
 			extra  VARCHAR(255) NOT NULL,
 			PRIMARY KEY (tln_fact_id),		
-			UNIQUE KEY tln_fact_uniq(tln_date_id, tln_time_id, tln_source_id, macb, description(255), filename(255), inode),
+			UNIQUE KEY tln_fact_uniq(tln_date_id, tln_time_id, tln_source_id, description(255), filename(255), inode),
 			FOREIGN KEY (tln_date_id) REFERENCES tln_date(tln_date_id),
 			FOREIGN KEY (tln_time_id) REFERENCES tln_time(tln_time_id),
 			FOREIGN KEY (tln_source_id) REFERENCES tln_source(tln_source_id)
@@ -228,9 +230,22 @@ class TlnData {
 	}
 	private function fill_source() {
 		$starttime = time();
-		$sql = "insert ignore into tln_source (source, sourcetype, type)\n"
-			. "select distinct source, sourcetype, type\n"
-			. "from tln_import";
+		$sql = 'insert ignore into tln_source (source, sourcetype, M, A, C, B)
+				select distinct source, sourcetype, mid(macb,1,1) as M, \'.\' as A, \'.\' as C, \'.\' as B
+    			from tln_import
+    			where macb like \'M___\' 
+    			union
+    			select distinct source, sourcetype, \'.\' as M, mid(macb,2,1) as A, \'.\' as C, \'.\' as B
+    			from tln_import
+    			where macb like \'_A__\' 
+    			union
+    			select distinct source, sourcetype, \'.\' as M, \'.\' as A, mid(macb,3,1) as C, \'.\' as B
+    			from tln_import
+    			where macb like \'__C_\' 
+    			union
+    			select distinct source, sourcetype, \'.\' as M, \'.\' as A, \'.\' as C, mid(macb,4,1) as B
+    			from tln_import
+    			where macb like \'___B\'';
 		if (! $this->db->query($sql)) {
 			print ('Error filling table: ' . $this->db->error . "\n");
 			return false;
@@ -241,10 +256,25 @@ class TlnData {
 	}
 	private function fill_fact() {
 		$starttime = time();
-		$sql = 'insert ignore into tln_fact(tln_date_id, tln_time_id, tln_source_id, count, macb, user, host, short, description, version, filename, inode, notes, format, extra)
-    			select d.tln_date_id, t.tln_time_id, s.tln_source_id, count(*), i.macb, i.user, i.host, i.short, i.description, i.version, i.filename, i.inode, i.notes, i.format, i.extra
+		$sql = 'insert ignore into tln_fact(tln_date_id, tln_time_id, tln_source_id, count, user, host, short, description, version, filename, inode, notes, format, extra)
+    			select d.tln_date_id, t.tln_time_id, s.tln_source_id, count(*), i.user, i.host, i.short, i.description, i.version, i.filename, i.inode, i.notes, i.format, i.extra
     			from tln_date d, tln_time t, tln_source s, tln_import i
-    			where d.date = i.date and t.tick = i.tick and s.source = i.source and s.sourcetype = i.sourcetype and s.type = i.type
+    			where d.date = i.date and t.tick = i.tick and s.source = i.source and s.sourcetype = i.sourcetype and s.M = \'M\' and i.macb like \'M___\'
+    			group by d.tln_date_id, t.tln_time_id, s.tln_source_id, i.macb, substring(i.description,255), substring(i.filename,255), i.inode
+    			union
+    			select d.tln_date_id, t.tln_time_id, s.tln_source_id, count(*), i.user, i.host, i.short, i.description, i.version, i.filename, i.inode, i.notes, i.format, i.extra
+    			from tln_date d, tln_time t, tln_source s, tln_import i
+    			where d.date = i.date and t.tick = i.tick and s.source = i.source and s.sourcetype = i.sourcetype and s.A = \'A\' and i.macb like \'_A__\'
+    			group by d.tln_date_id, t.tln_time_id, s.tln_source_id, i.macb, substring(i.description,255), substring(i.filename,255), i.inode
+    			union
+    			select d.tln_date_id, t.tln_time_id, s.tln_source_id, count(*), i.user, i.host, i.short, i.description, i.version, i.filename, i.inode, i.notes, i.format, i.extra
+    			from tln_date d, tln_time t, tln_source s, tln_import i
+    			where d.date = i.date and t.tick = i.tick and s.source = i.source and s.sourcetype = i.sourcetype and s.C = \'C\' and i.macb like \'__C_\'
+    			group by d.tln_date_id, t.tln_time_id, s.tln_source_id, i.macb, substring(i.description,255), substring(i.filename,255), i.inode
+    			union
+    			select d.tln_date_id, t.tln_time_id, s.tln_source_id, count(*), i.user, i.host, i.short, i.description, i.version, i.filename, i.inode, i.notes, i.format, i.extra
+    			from tln_date d, tln_time t, tln_source s, tln_import i
+    			where d.date = i.date and t.tick = i.tick and s.source = i.source and s.sourcetype = i.sourcetype and s.B = \'B\' and i.macb like \'___B\'
     			group by d.tln_date_id, t.tln_time_id, s.tln_source_id, i.macb, substring(i.description,255), substring(i.filename,255), i.inode';
 		if (! $this->db->query($sql)) {
 			print('Error filling table: ' . $this->db->error . "\n");
@@ -326,13 +356,12 @@ class TlnData {
 					// Insert the fact table rows
 					if ($this->fill_fact()) {
 						// Empty the imported text 
-						if ($this->empty_import($job->getId())) {
+						// if ($this->empty_import($job->getId())) {
 							// COMMIT
 							if ($this->commit($job->getId())) {
-									
 								return true;		
-							}
-						}
+							 }
+						// }
 					}	
 				} 
 			}
@@ -356,17 +385,120 @@ class TlnData {
 		return true;
 	}
 	function get() {
-		
+		$starttime = time();
+		$sql = 'select d.year, d.month, s.sourcetype, s.M, s.A, s.C, s.B, sum(f.count) as items
+    			from tln_date d inner join (
+    			tln_source s inner join tln_fact f on s.tln_source_id = f.tln_source_id
+    			) on d.tln_date_id = f.tln_date_id
+    			group by d.year, d.month, s.sourcetype, s.M, s.A, s.C, s.B
+    			order by d.year desc, d.month desc, s.sourcetype, s.M, s.A, s.C, s.B';
+		$result = array();
+		if ($stmt = $this->db->prepare($sql)) {
+			$stmt->execute();
+			$stmt->bind_result($year, $month, $source, $m, $a, $c, $b, $items);
+			$columns = array();
+			$sourcerows = array();
+			while ($stmt->fetch()) {
+				if (( ! isset($oldyear)) || $year == $oldyear) {
+					$params['year'] = $year;
+					if (( ! isset($oldmonth)) || $month == $oldmonth) {
+						$params['month'] = $month;
+						if (( ! isset($oldsource)) || $source == $oldsource) {
+							$params['source'] = $source;
+							$this->columns($columns, $m, $a, $c, $b, $items, $params);
+							$oldsource = $source;
+						} else {
+							$sourcerows[] = $this->sourcerows($columns, $oldsource, $params);
+							$columns = array();
+							$params['source'] = $source;
+							$this->columns($columns, $m, $a, $c, $b, $items, $params);
+							$oldsource = $source;
+						}
+						$oldmonth = $month;
+					} else {
+						$sourcerows[] = $this->sourcerows($columns, $oldsource, $params);
+						unset($params['source']);
+						$result[] = $this->daterows($sourcerows, $year, $oldmonth, $params);
+						$columns = array();
+						$sourcerows = array();
+						$params['source'] = $source;
+						$params['month'] = $month;
+						$this->columns($columns, $m, $a, $c, $b, $items, $params);
+						$oldsource = $source;
+						$oldmonth = $month;
+					}
+					$oldyear = $year;
+				} else {
+					$sourcerows[] = $this->sourcerows($columns, $oldsource, $params);
+					unset($params['source']);
+					$result[]  = $this->daterows($sourcerows, $oldyear, $oldmonth, $params);
+					$columns = array();
+					$sourcerows = array();
+					$params['source'] = $source;
+					$params['month'] = $month;
+					$params['year'] = $year;
+					$this->columns($columns, $m, $a, $c, $b, $items, $params);
+					$oldsource = $source;
+					$oldmonth = $month;
+					$oldyear = $year;
+				}
+			}
+			$sourcerows[] = $this->sourcerows($columns, $oldsource, $params);
+			unset($params['source']);
+			$result[]  = $this->daterows($sourcerows, $oldyear, $oldmonth, $params);
+			return $result;
+		}
+	}
+	private function daterows (&$rows, $year, $month, $params) {
+		return array($month . '/' . $year, $rows, $params);
+	}
+	private function sourcerows (&$columns, $source, $params) { 
+		return array($source, $columns, $params);
+	}
+	private function columns(&$columns, $m, $a, $c, $b, $items, $params) {
+		$myparams = $params; 
+		if ($m == 'M') {
+			$myparams['macb'] = 'M';
+			$columns['M'] = array($items, $myparams);
+		}
+		if ($a == 'A') {
+			$myparams['macb'] = 'A';
+			$columns['A'] = array($items, $myparams);
+		}
+		if ($c == 'C') {
+			$myparams['macb'] = 'C';
+			$columns['C'] = array($items, $myparams);
+		}
+		if ($b == 'B') {
+			$myparams['macb'] = 'B';
+			$columns['B'] = array($items, $myparams);
+		}
+		unset($myparams['macb']);
+		$columns['total'] = array($columns['total'][0] + $items, $myparams);
+		return $columns;
 	}
 	function get_proportional() {
+		$job = Job::get_new();
 		$starttime = time();
-		$sql = 'select s.source, d.year, d.month, count(f.count)
+		$sql = 'select s.source, d.year, d.month, count(f.count) as items
     			from tln_date d left join (
     			tln_source s inner join tln_fact f on s.tln_source_id = f.tln_source_id
     			) on d.tln_date_id = f.tln_date_id
     			where d.year > 1980 and d.year <= 2011
     			group by s.source, d.year, d.month
     			order by s.source, d.year, d.month';
+		if ($stmt = $this->db->prepare($sql)) {
+			$stmt->execute();
+			$stmt->bind_result($source, $year, $month, $count, $items);
+			if ($stmt->fetch()) {
+				$job->setTask($task);
+				$job->setStatus($status);
+				$job->setPercent($percent);
+				$job->setSaved(true);
+			} else {
+				Job::save($this->db, $job);
+			}
+		}
 	}
 	private function begin() {
 		$insert = 'start transaction';
@@ -404,6 +536,12 @@ class TlnData {
 		if (! preg_match('@^\d+/\d+/\d+,\d+:\d+:\d+,\w+,[M.][A.][C.][B.],[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+$@', $input, $match)) 
 			return false;
 		return true;
+	}
+	function h2q($my_params) {
+		foreach ($my_params as $k => $v) {
+				$query[] = $k . '=' . $v;
+		}
+		return implode('&', $query);
 	}
 }
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once('tln-config.php');
 require_once('TlnData.php');
 
@@ -6,13 +7,64 @@ $db = new mysqli(TLNDBHOST, TLNDBUSER, TLNDBPASS, TLNDBNAME);
 if (mysqli_connect_errno()) {
 	die('Database connection error: ' . mysqli_connect_error() . '  Please check \'tfx-config.php\'');
 }
-$xmlDoc = new DOMDocument();
-$xmlDoc->loadXML(@file_get_contents('php://input'));
-$nodes = $xmlDoc->getElementsByTagName("Data");
-if ($nodes->length > 0) {
-	set_time_limit(120);
-	$tln = new TlnData($db);
-	$tln->import($nodes->item(0)->textContent);
+
+$tln = new TlnData($db);
+$input = file_get_contents('php://input');
+if ($input) {
+	$xmlDoc = new DOMDocument();
+	$xmlDoc->loadXML(@$input);
+	$nodes = $xmlDoc->getElementsByTagName("Data");
+	if ($nodes->length > 0) {
+		set_time_limit(120);
+		$tln->import($nodes->item(0)->textContent);
+	}
+} else {
+	$result = $tln->get();
+	print "<html><body><table><tbody> 
+		<thead>
+	    <tr>
+	      <th>Date Time</th>
+	      <th>Log Source</th>
+	      <th>M</th>
+	      <th>A</th>
+	      <th>C</th>
+	      <th>B</th>
+	      <th>Total</th>
+	    </tr>
+	  	</thead>\n";
+	foreach ($result as $daterow) {
+		print '<tr><td rowspan="' . count($daterow[1]) . '" ><a href="' . $tln->h2q($daterow[2]) . '" >' . $daterow[0] . '</a></td>';
+		foreach ($daterow[1] as $sourcerow) {
+			print '<td><a href="' . $tln->h2q($sourcerow[2]) . '" >' . $sourcerow[0] . '</a></td>';
+			if (array_key_exists('M', $sourcerow[1])) {
+				print '<td><a href="?' . $tln->h2q($sourcerow[1]['M'][1]) . '" >' . $sourcerow[1]['M'][0] . '</a></td>';
+			} else {
+				print '<td>0</td>';
+			}
+			if (array_key_exists('A', $sourcerow[1])) {
+				print '<td><a href="?' . $tln->h2q($sourcerow[1]['A'][1]) . '" >' . $sourcerow[1]['A'][0] . '</a></td>';
+			} else {
+				print '<td>0</td>';
+			}
+			if (array_key_exists('C', $sourcerow[1])) {
+				print '<td><a href="?' . $tln->h2q($sourcerow[1]['C'][1]) . '" >' . $sourcerow[1]['C'][0] . '</a></td>';
+			} else {
+				print '<td>0</td>';
+			}
+			if (array_key_exists('B', $sourcerow[1])) {
+				print '<td><a href="?' . $tln->h2q($sourcerow[1]['B'][1]) . '" >' . $sourcerow[1]['B'][0] . '</a></td>';
+			} else {
+				print '<td>0</td>';
+			}
+			if (array_key_exists('total', $sourcerow[1])) {
+				print '<td><a href="?' . $tln->h2q($sourcerow[1]['total'][1]) . '" >' . $sourcerow[1]['total'][0] . '</a></td>';
+			} else {
+				print '<td>0</td>';
+			}
+			print '</tr>';
+		}
+	}
+	print "</tbody></table></body></html>\n";
 }
 
 $db->close();
